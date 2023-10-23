20187fe4cc9cc45e07d72b694ba44703d8dacf5f
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2023-10-23 18:58:22 +1300

    No need to explicitly import React.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,9 +7,9 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
